2dee2148e3b808ab96416a1b7aea2fd9c038b7e0
Meet <mkds622@gmail.com>
2017-09-08 19:00:24 +0530

    /React/to-do/ => Generation containers with connect() from React Redux(FooterLink, which uses state items in both props as well as dispatch).

--- Redux/to-do/src/index.js
+++ Redux/to-do/src/index.js
@@ -19,33 +19,32 @@ const Link = ({ active, children, onClick }) => {
     );
 };
 
-class FilterLink extends React.Component {
-    componentDidMount(){
-        const {store} = this.context;
-        this.unsubscribe=store.subscribe(()=>this.forceUpdate());
-    }
-    componentWillUnmount(){
-        this.unsubscribe();
-    }
-    render() {
-        const props=this.props;
-        const {store} = this.context;
-        const state = store.getState();
+const mapStateToLinkProps =(
+    state,
+    ownProps
+) => {
+    return {
+        active:ownProps.filter === state.visibilityFilter
+    };
+}
+const mapDispatchToLinkProps = (
+    dispatch,
+    ownProps
+) =>{
+    return {
+        onClick: ()=>{
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter : ownProps.filter
+            })
+        }
+    };
+}
+const FilterLink = connect(
+    mapStateToLinkProps,
+    mapDispatchToLinkProps
+)(Link);
 
-        return (
-            <Link
-                active = {props.filter === state.visibilityFilter}
-                onClick= {()=> store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER',
-                    filter : props.filter
-                })}
-                >{props.children}</Link>
-        );
-    }
-}
-FilterLink.contextTypes = {
-    store : React.PropTypes.Object
-}
 
 const Todo = ({
     onClick,
